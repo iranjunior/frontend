6748b483302c11b8d5a4ee8073c2198faa0815a1
iran junior <iranjunior94@gmail.com>
2019-07-19 22:46:17 -0300

    primeiras coisas funcionando

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../../Components/Header'
+// import { Container } from './styles';
+
+export default class Home extends Component {
+    
+    render() {
+        return (
+            <Header></Header>
+        );
+  }
+}
