041ac49144371a931548d2015684b392859cc371
iranjunior94 <iranjunior94@gmail.com>
2019-07-29 08:26:47 -0300

    resolvendo conflitos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import { connect } from 'react-redux';
 
 import Routes  from './Routes';
->>>>>>> 63cd01a06a06c7f0333d67b6947d6e08b1e53b99
 
 
 function App() {
   return (
-<<<<<<< HEAD
-    <h1>
-      Olá Mundo
-    </h1>
-=======
           <Routes />
   
->>>>>>> 63cd01a06a06c7f0333d67b6947d6e08b1e53b99
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-=======
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { Route } from 'react-router'; 
->>>>>>> 63cd01a06a06c7f0333d67b6947d6e08b1e53b99
 
 import App from './App';
 import store   from './Store';
