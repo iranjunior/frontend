1a288b3288508cf6e9f1e86a91a8ebe8c83b5d34
iran junior <iranjunior94@gmail.com>
2019-08-04 15:13:37 -0300

    mapa funcionando mas sem mapa de calor

--- src/Components/Maps/index.js
+++ src/Components/Maps/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, Circle } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
+import HeatmapLayer from "react-google-maps/lib/components/visualization/HeatmapLayer";
 import mapStyles from './mapStyles';
 
 import { Container, FieldFilter, MapField } from "./styles";
@@ -12,11 +13,12 @@ const Map = withScriptjs(withGoogleMap((props) =>
     defaultOptions={{styles: mapStyles}}
   >
     
-    <Circle defaultCenter={{ lat: -8.305095, lng: -37.777673 }} radius={30000} options={{
-          fillColor: '#34be74',
-          fillOpacity: .2,
-          strokeColor: '#2faa68',
-          strokeWeight: 0.4}} />
+      <HeatmapLayer
+      data={[
+        {location: {lat: -8.0421584, lng: -35.008676}, weight: 0.5}
+      ]}
+      options={{radius: 30000}}
+      />
   </GoogleMap>
 ))
 
@@ -27,7 +29,7 @@ const Maps = () => (
       <FieldFilter></FieldFilter>
     <MapField>
     <Map
-        googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCeYzwunekz59YFjsVKp8xO-K-agkgvOUA"}
+        googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization&key=AIzaSyCeYzwunekz59YFjsVKp8xO-K-agkgvOUA"}
         loadingElement={<div style={{ height: `100%`, position: `unset` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `400px` }} />}
