37908dffc4b300c5299a165e3d42a106668cf313
iran junior <iranjunior94@gmail.com>
2019-08-17 21:28:03 -0300

    inicio do uso do eslint

--- src/Components/Panel/index.js
+++ src/Components/Panel/index.js
@@ -1,24 +1,24 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import api, { config } from "../../Services/api";
+/* eslint-disable react/prop-types */
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { IconContext } from 'react-icons';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import Chip from '@material-ui/core/Chip';
+import api, { config } from '../../Services/api';
 import {
   LOADED_VACANCIES,
   SET_SPECIALITIES,
   LOADED_SPECIALITIES,
   UIDESIGN_CLICKED_SELECT_ORDER_VACANCIES,
   ORDER_VACANCIES,
-  SETTING_HOSPITALS_NAME
-} from "../../Constants/actionsType";
-//import { MdWhatshot } from 'react-icons/md'
-import { IconContext } from "react-icons";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import Checkbox from "@material-ui/core/Checkbox";
-import Chip from "@material-ui/core/Chip";
+  SETTING_HOSPITALS_NAME,
+} from '../../Constants/actionsType';
 import {
   Container,
   Hospital,
@@ -35,104 +35,98 @@ import {
   LabelSelect,
   Tittle,
   useStyles,
-  getIcons
-} from "./styles";
+  getIcons,
+} from './styles';
 
 const loadVacancies = (vacancies, dispatch) => {
-  if (
-    vacancies === null ||
-    vacancies === undefined ||
-    Object.keys(vacancies).length === 0
-  ) {
+  if (vacancies === null || vacancies === undefined || Object.keys(vacancies).length === 0) {
     api
-      .get("/hospitals", config)
-      .then(response => {
+      .get('/hospitals', config)
+      .then((response) => {
         if (response.status === 200) {
           dispatch({
             type: LOADED_VACANCIES,
-            vacancies: response.data.hospitals
+            vacancies: response.data.hospitals,
           });
-          
           dispatch({
             type: SETTING_HOSPITALS_NAME,
-            hospitals: [...new Set(response.data.hospitals.map(el => el.hospital).sort())]
+            hospitals: [...new Set(response.data.hospitals.map((el) => el.hospital).sort())],
           });
-
         }
       })
-      .catch(erro => {});
+      .catch((erro) => {
+        throw erro;
+      });
   }
 };
 const loadSpecialities = (specialities, dispatch) => {
   if (
-    specialities === null ||
-    specialities === undefined ||
-    Object.keys(specialities).length === 0
+    specialities === null || specialities === undefined || Object.keys(specialities).length === 0
   ) {
     api
-      .get("/speciality", config)
-      .then(response => {
+      .get('/speciality', config)
+      .then((response) => {
         if (response.status === 200) {
           dispatch({
             type: LOADED_SPECIALITIES,
-            specialities: [
-              ...new Set(response.data.specialties.map(el => el.name).sort())
-            ]
+            specialities: [...new Set(response.data.specialties.map((el) => el.name).sort())],
           });
         }
       })
-      .catch(erro => {});
+      .catch((erro) => {
+        throw erro;
+      });
   }
 };
 
-const handleChangeFilter = e => ({
+const handleChangeFilter = (e) => ({
   type: SET_SPECIALITIES,
-  specialities: e.target.value
+  specialities: e.target.value,
 });
 const handleDeleteFilter = (value, setSpecialities) => ({
   type: SET_SPECIALITIES,
-  specialities: setSpecialities.filter(el => el !== value)
+  specialities: setSpecialities.filter((el) => el !== value),
 });
-const handleChangeOrder = e => ({
+const handleChangeOrder = (e) => ({
   type: UIDESIGN_CLICKED_SELECT_ORDER_VACANCIES,
-  value: e.target.value
+  value: e.target.value,
 });
 const reorderVacancies = (vacancies, selectedOrderVacancy, dispatch) => {
   dispatch({
     type: ORDER_VACANCIES,
-    order: selectedOrderVacancy
+    order: selectedOrderVacancy,
   });
-  if (selectedOrderVacancy === "DESC") {
-    let reorderVacancy = vacancies.sort((a, b) => {
+  if (selectedOrderVacancy === 'DESC') {
+    const reorderVacancy = vacancies.sort((a, b) => {
       if (a.vacancy < b.vacancy) return 1;
-      else if (a.vacancy > b.vacancy) return -1;
-      else return 0;
+      if (a.vacancy > b.vacancy) return -1;
+      return 0;
     });
     dispatch({
       type: LOADED_VACANCIES,
-      vacancies: reorderVacancy
+      vacancies: reorderVacancy,
     });
-  } else if (selectedOrderVacancy === "ASC") {
-    let reorderVacancy = vacancies
+  } else if (selectedOrderVacancy === 'ASC') {
+    const reorderVacancy = vacancies
       .sort((a, b) => {
         if (a.vacancy < b.vacancy) return 1;
-        else if (a.vacancy > b.vacancy) return -1;
-        else return 0;
+        if (a.vacancy > b.vacancy) return -1;
+        return 0;
       })
       .reverse();
     dispatch({
       type: LOADED_VACANCIES,
-      vacancies: reorderVacancy
+      vacancies: reorderVacancy,
     });
   } else {
-    let reorderVacancy = vacancies.sort((a, b) => {
+    const reorderVacancy = vacancies.sort((a, b) => {
       if (a.hospital > b.hospital) return 1;
-      else if (a.hospital < b.hospital) return -1;
-      else return 0;
+      if (a.hospital < b.hospital) return -1;
+      return 0;
     });
     dispatch({
       type: LOADED_VACANCIES,
-      vacancies: reorderVacancy
+      vacancies: reorderVacancy,
     });
   }
 };
@@ -142,9 +136,9 @@ const MenuProps = {
   PaperProps: {
     style: {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250
-    }
-  }
+      width: 250,
+    },
+  },
 };
 const Panel = ({
   vacancies,
@@ -152,14 +146,15 @@ const Panel = ({
   uiDesign,
   specialities,
   setSpecialities,
-  dispatch
+  dispatch,
 }) => {
   const classes = useStyles();
   useEffect(() => {
     loadVacancies(vacancies, dispatch);
     loadSpecialities(specialities, dispatch);
-    if (orderVacancies !== uiDesign.selectedOrderVacancy)
+    if (orderVacancies !== uiDesign.selectedOrderVacancy) {
       reorderVacancies(vacancies, uiDesign.selectedOrderVacancy, dispatch);
+    }
   });
 
   return (
@@ -169,12 +164,12 @@ const Panel = ({
           <LabelSelect>
             <Tittle>Filtros Usados</Tittle>
           </LabelSelect>
-          {setSpecialities.map(value => (
+          {setSpecialities.map((value) => (
             <Chip
               key={value}
               className={classes.chips}
               label={value}
-              onDelete={e => {
+              onDelete={() => {
                 dispatch(handleDeleteFilter(value, setSpecialities));
               }}
             />
@@ -185,20 +180,18 @@ const Panel = ({
             <Tittle className={classes.noMargin}>Filtros</Tittle>
           </LabelSelect>
           <FormControl className={classes.formcontrol}>
-            <InputLabel htmlFor="select-multiple-checkbox">
-              Especialidades
-            </InputLabel>
+            <InputLabel htmlFor="select-multiple-checkbox">Especialidades</InputLabel>
             <Select
               multiple
               value={setSpecialities}
-              onChange={e => {
+              onChange={(e) => {
                 dispatch(handleChangeFilter(e));
               }}
               input={<Input id="select-multiple-checkbox" />}
-              renderValue={selected => selected.join(", ")}
+              renderValue={(selected) => selected.join(', ')}
               MenuProps={MenuProps}
             >
-              {specialities.map(el => (
+              {specialities.map((el) => (
                 <MenuItem key={el} value={el}>
                   <Checkbox checked={setSpecialities.indexOf(el) > -1} />
                   <ListItemText primary={el} />
@@ -215,20 +208,20 @@ const Panel = ({
             <InputLabel htmlFor="select-order">Ordernar por</InputLabel>
             <Select
               value={uiDesign.selectedOrderVacancy}
-              onChange={e => {
+              onChange={(e) => {
                 dispatch(handleChangeOrder(e));
               }}
               input={<Input id="select-order" />}
             >
-              <MenuItem value={"none"}>Hospitais</MenuItem>
-              <MenuItem value={"DESC"}>Decrescente</MenuItem>
-              <MenuItem value={"ASC"}>Crescente</MenuItem>
+              <MenuItem value="none">Hospitais</MenuItem>
+              <MenuItem value="DESC">Decrescente</MenuItem>
+              <MenuItem value="ASC">Crescente</MenuItem>
             </Select>
           </FormControl>
         </FilterOrder>
       </FilterField>
       <PanelField>
-        {vacancies.map(el => {
+        {vacancies.map((el) => {
           if (setSpecialities.length === 0) {
             return (
               <Card vacancies={el.vacancy} key={el.key.toString()}>
@@ -237,14 +230,14 @@ const Panel = ({
                   <Icon>
                     <IconContext.Provider
                       value={{
-                        color: "#fff",
-                        size: "2em",
-                        style: { margin: "10px" }
+                        color: '#fff',
+                        size: '2em',
+                        style: { margin: '10px' },
                       }}
                     >
                       {getIcons(el.speciality)}
                     </IconContext.Provider>
-                  {el.speciality}
+                    {el.speciality}
                   </Icon>
                 </Specialities>
                 <Vacancies>
@@ -253,7 +246,8 @@ const Panel = ({
                 </Vacancies>
               </Card>
             );
-          } else if (setSpecialities.includes(el.speciality)) {
+          }
+          if (setSpecialities.includes(el.speciality) === true) {
             return (
               <Card vacancies={el.vacancy} key={el.key.toString()}>
                 <Hospital>{el.hospital}</Hospital>
@@ -261,36 +255,37 @@ const Panel = ({
                   <Icon>
                     <IconContext.Provider
                       value={{
-                        color: "#fff",
-                        size: "2em",
-                        style: { margin: "10px" }
+                        color: '#fff',
+                        size: '2em',
+                        style: { margin: '10px' },
                       }}
-                      >
+                    >
                       {getIcons(el.speciality)}
-
                     </IconContext.Provider>
-                  {el.speciality}
+                    {el.speciality}
                   </Icon>
                 </Specialities>
                 <Vacancies>
                   {el.vacancy}
                   <Name>Leitos</Name>
                 </Vacancies>
-                
               </Card>
             );
           }
+          return null;
         })}
       </PanelField>
     </Container>
   );
 };
 
-export default connect(state => ({
+const mapStateToProps = (state) => ({
   ...state,
   uiDesign: state.uiDesign.panel,
   specialities: state.specialities.specialities,
   setSpecialities: state.specialities.setSpecialities,
   vacancies: state.vacancies.vacancies,
-  orderVacancies: state.vacancies.order
-}))(Panel);
+  orderVacancies: state.vacancies.order,
+});
+
+export default connect(mapStateToProps)(Panel);
